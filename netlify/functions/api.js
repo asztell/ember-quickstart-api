78787f6c1b8b38c30e8da73ee246a42c617dcda1
Arpad <arpad@Arpads-MacBook-Pro.local>
2024-04-18 18:58:30 -0400

    clean up

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,4 @@
 var serverless = require('serverless-http');
 var app = require('../../app');
-console.log('app', app);
-// const handler = serverless(app);
 
 module.exports.handler = serverless(app);
